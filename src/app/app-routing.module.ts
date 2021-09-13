1cdafbf47601c6d551a97c86496c890c6f7acade
destructiv33 <osmaritur@gmail.com>
2021-09-12 21:37:55 -0500

    css files navbar routes bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule),},
+  { path: 'about', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule) },
+  { path: 'portfolio', loadChildren: () => import('./pages/portfolio/portfolio.module').then(m => m.PortfolioModule) },
+  { path: 'skills', loadChildren: () => import('./pages/skills/skills.module').then(m => m.SkillsModule) }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
